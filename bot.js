54827bf730e1d57bb281ac5e4cf5a232ac8251a3
Oleksandr Bezditnyi <oleksandrbezditny@gmail.com>
2024-03-25 21:12:25 +0100

    Github workflow

---
+++ bot.js
@@ -0,0 +1,19 @@
+import {Telegraf, Markup} from 'telegraf';
+
+const token = '6852025342:AAEE2KUjCfuVqRNqwOPL9LeaTj19oDWvjc4';
+const webAppUrl = 'https://google.com/';
+
+const bot = new Telegraf(token);
+
+bot.command('start', (ctx) => {
+    ctx.reply('Hello, to start application, please, press button below',
+        Markup.keyboard([
+            Markup.button.webApp(
+                'Send message',
+                webAppUrl,
+            )
+        ])
+    );
+});
+
+bot.launch();
